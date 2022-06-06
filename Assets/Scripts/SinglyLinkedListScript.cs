211726b326e74e9be10c0bef2997c6f2e4a47625
Detjon <detjon.mataj@gmail.com>
2022-06-06 03:05:32 +0200

    Implemented Add Value at Given Possition in Linked List Also fixed some bugs for some special casess

--- Assets/Scripts/SinglyLinkedListScript.cs
+++ Assets/Scripts/SinglyLinkedListScript.cs
@@ -37,7 +37,6 @@ public class SinglyLinkedListScript : MonoBehaviour
     void Start()
     {
         InitializeRandom();
-        DeleteAtPosition();
     }
 
     public void InitializeRandom()
@@ -231,8 +230,8 @@ public class SinglyLinkedListScript : MonoBehaviour
 
     public void Prepend()
     {
-        int number = int.Parse(NumberInputField.text);
-        StartCoroutine(PrependUtil(number));
+        // int number = int.Parse(NumberInputField.text);
+        StartCoroutine(PrependUtil(1));
     }
 
     private IEnumerator AppendUtil(int number)
@@ -383,7 +382,7 @@ public class SinglyLinkedListScript : MonoBehaviour
             Destroy(newNodeLabel);
 
 
-            for (int i = length - 1; i > 0; i--)
+            for (int i = length; i > 0; i--)
             {
                 nodes[i] = nodes[i - 1];
                 links[i] = links[i - 1];
@@ -517,6 +516,7 @@ public class SinglyLinkedListScript : MonoBehaviour
         } else if (int.Parse(nodes[0].GetComponentInChildren<TextMeshPro>().text) == number)
         {
             DeleteFirst();
+            yield break;
         }
         else
         {
@@ -645,12 +645,12 @@ public class SinglyLinkedListScript : MonoBehaviour
 
     public void DeleteAtPosition()
     {
-        int number = int.Parse(IndexInputField.text);
-        if (number == 0 || number > length) return;
-        StartCoroutine(DeleteAtPositionUtil(number-1));
+        int position = int.Parse(IndexInputField.text);
+        if (position == 0 || position > length) return;
+        StartCoroutine(DeleteAtPositionUtil(position - 1));
     }
 
-    private IEnumerator DeleteAtPositionUtil(int number)
+    private IEnumerator DeleteAtPositionUtil(int position)
     {
 
 
@@ -658,9 +658,10 @@ public class SinglyLinkedListScript : MonoBehaviour
         {
 
         }
-        else if (number == 0)
+        else if (position == 0)
         {
             DeleteFirst();
+            yield break;
         }
         else
         {
@@ -716,7 +717,7 @@ public class SinglyLinkedListScript : MonoBehaviour
                     LeanTween.moveLocalX(previousLink, nodes[i - 1].transform.localPosition.x, .9f);
                 }
 
-                if (i == number)
+                if (i == position)
                 {
                     yield return new WaitForSeconds(1.1f);
                     LeanTween.moveLocalY(nodes[i], nodes[i].transform.localPosition.y + 0.5f, 1f);
@@ -783,6 +784,173 @@ public class SinglyLinkedListScript : MonoBehaviour
             }
 
             length--;
+        }
+    }
+
+
+    public void InsertAtPosition()
+    {
+        int number = int.Parse(NumberInputField.text);
+        int position = int.Parse(IndexInputField.text);
+        if (position == 0 || position > length + 1) return;
+        StartCoroutine(InsertAtPositionUtil(number, position-1));
+    }
+
+    private IEnumerator InsertAtPositionUtil(int number, int position)
+    {
+
+
+        if (length == 0)
+        {
+
+        }
+        else if (position == 0)
+        {
+            Prepend();
+            yield break;
+        } else if(position == length) {
+            Append();
+            yield break;
+        }
+        else
+        {
+            GameObject CurrentLabel;
+            GameObject CurrentLink;
+            GameObject previousLink;
+            GameObject previousLabel;
+            GameObject newNode;
+            GameObject newNodeLabel;
+            GameObject newNodeLinker;
+
+            previousLabel = Instantiate(PreviousLBL, new Vector3(nodes[0].transform.localPosition.x, nodes[0].transform.localPosition.y - .9f, 0), Quaternion.identity);
+            previousLabel.transform.localScale = new Vector3(1.2f, 1.2f, 0.001f);
+            previousLabel.transform.SetParent(this.transform);
+            previousLink = Instantiate(link, new Vector3(nodes[0].transform.localPosition.x, nodes[0].transform.localPosition.y - 0.55f, 0), Quaternion.identity);
+            previousLink.transform.Rotate(0.0f, 0.0f, 90.0f);
+            previousLink.transform.localScale = new Vector3(0.2f, 0.2f, 0.001f);
+            previousLink.transform.SetParent(this.transform);
+            previousLabel.transform.localPosition = new Vector3(nodes[0].transform.localPosition.x, nodes[0].transform.localPosition.y - .9f, 0);
+            previousLink.transform.localPosition = new Vector3(nodes[0].transform.localPosition.x, nodes[0].transform.localPosition.y - 0.55f, 0);
+
+            previousLabel.SetActive(false);
+            previousLink.SetActive(false);
+
+            CurrentLabel = GameObject.Instantiate(CurrentText, new Vector3(nodes[0].transform.localPosition.x, nodes[0].transform.localPosition.y - .9f, 0), Quaternion.identity);
+            CurrentLabel.transform.localScale = new Vector3(1.2f, 1.2f, 0.001f);
+            CurrentLabel.transform.SetParent(this.transform);
+            CurrentLink = GameObject.Instantiate(link, new Vector3(nodes[0].transform.localPosition.x, nodes[0].transform.localPosition.y - 0.55f, 0), Quaternion.identity);
+            CurrentLink.transform.Rotate(0.0f, 0.0f, 90.0f);
+            CurrentLink.transform.localScale = new Vector3(0.2f, 0.2f, 0.001f);
+            CurrentLink.transform.SetParent(this.transform);
+            CurrentLabel.transform.localPosition = new Vector3(nodes[0].transform.localPosition.x, nodes[0].transform.localPosition.y - .9f, 0);
+            CurrentLink.transform.localPosition = new Vector3(nodes[0].transform.localPosition.x, nodes[0].transform.localPosition.y - 0.55f, 0);
+
+            yield return new WaitForSeconds(1);
+            int i;
+
+            for (i = 0; i < length; i++)
+            {
+                LeanTween.moveLocalX(CurrentLabel, nodes[i].transform.localPosition.x, .7f);
+                LeanTween.moveLocalX(CurrentLink, nodes[i].transform.localPosition.x, .7f);
+                LeanTween.color(nodes[i], Color.blue, 1f).setLoopPingPong(1);
+                LeanTween.moveLocalX(links[i], links[i].transform.localPosition.x + 0.09f, 1f).setLoopPingPong(1);
+
+                if (i == 1)
+                {
+                    yield return new WaitForSeconds(.8f);
+                    previousLabel.SetActive(true);
+                    previousLink.SetActive(true);
+                }
+
+                if (i > 1)
+                {
+                    LeanTween.moveLocalX(previousLabel, nodes[i - 1].transform.localPosition.x, .9f);
+                    LeanTween.moveLocalX(previousLink, nodes[i - 1].transform.localPosition.x, .9f);
+                }
+
+                if (i == position || i == length-1)
+                {
+                    yield return new WaitForSeconds(1.1f);
+
+                    newNode = Instantiate(node, new Vector3(nodes[i].transform.localPosition.x, nodes[i].transform.localPosition.y + 1.2f, 0), Quaternion.identity);
+                    newNode.transform.SetParent(this.transform);
+                    newNode.transform.localScale = new Vector3(0.6f, 0.6f, 0.001f);
+                    newNode.GetComponentInChildren<TextMeshPro>().text = number.ToString();
+                    newNode.GetComponentInChildren<TextMeshPro>().color = Color.black;
+                    newNode.transform.localPosition = new Vector3(nodes[i].transform.localPosition.x, nodes[i].transform.localPosition.y + 1.2f, 0);
+
+                    newNodeLabel = Instantiate(newNodeText, new Vector3(newNode.transform.localPosition.x, newNode.transform.localPosition.y + .5f, 0), Quaternion.identity);
+                    newNodeLabel.transform.localScale = new Vector3(.9f, .9f, 0.001f);
+                    newNodeLabel.transform.SetParent(this.transform);
+                    newNodeLabel.transform.localPosition = new Vector3(newNode.transform.localPosition.x, newNode.transform.localPosition.y + .5f, 0);
+
+                    newNodeLinker = Instantiate(link, new Vector3(newNode.transform.localPosition.x, 1.6f, 0), Quaternion.identity);
+                    newNodeLinker.transform.Rotate(0.0f, 0.0f, -90.0f);
+                    newNodeLinker.transform.localScale = new Vector3(0.2f, 0.2f, 0.001f);
+                    newNodeLinker.transform.SetParent(this.transform);
+                    newNodeLinker.transform.localPosition = new Vector3(newNode.transform.localPosition.x, 1.6f, 0);
+
+                    yield return new WaitForSeconds(1);
+
+                    LeanTween.rotateZ(links[i - 1], 50f, 1f).setLoopPingPong(1);
+                    LeanTween.moveLocalX(newNodeLinker, links[i].transform.localPosition.x, 1.2f);
+                    LeanTween.moveLocalY(newNodeLinker, links[i].transform.localPosition.y, 1.2f);
+                    LeanTween.rotateZ(newNodeLinker, 0, 1.2f);
+                    LeanTween.moveLocalX(newNode, nodes[i].transform.localPosition.x, 1.2f);
+                    LeanTween.moveLocalY(newNode, nodes[i].transform.localPosition.y, 1.2f);
+                    LeanTween.moveLocalX(newNodeLabel, nodes[i].transform.localPosition.x, 1.2f);
+                    LeanTween.moveLocalY(newNodeLabel, nodes[i].transform.localPosition.y + .5f, 1.2f);
+
+                    for (int j = length; j > i; j--)
+                    {
+                        nodes[j] = nodes[j - 1];
+                        links[j] = links[j - 1];
+                        if (j == length)
+                        {
+                            LeanTween.moveLocalX(TailLabel, TailLabel.transform.localPosition.x + 1.2f, 1f);
+                            LeanTween.moveLocalX(TailLink, TailLink.transform.localPosition.x + 1.2f, 1f);
+                            LeanTween.moveLocalX(NullText, NullText.transform.localPosition.x + 1.2f, 1f);
+                        } else if (j == i+1)
+                        {
+                            LeanTween.moveLocalX(CurrentLabel, nodes[j+1].transform.localPosition.x, 1.2f);
+                            LeanTween.moveLocalX(CurrentLink, nodes[j+1].transform.localPosition.x, 1.2f);
+                        }
+                        LeanTween.moveLocalX(nodes[j], nodes[j].transform.localPosition.x + 1.2f, 1f);
+                        LeanTween.moveLocalX(links[j], links[j].transform.localPosition.x + 1.2f, 1f);
+                    }
+                    yield return new WaitForSeconds(1.7f);
+                    newNodeLabel.transform.SetParent(null);
+                    Destroy(newNodeLabel);
+
+                    nodes[i] = newNode;
+                    links[i] = newNodeLinker;
+
+                    CurrentLabel.transform.SetParent(null);
+                    CurrentLink.transform.SetParent(null);
+                    Destroy(CurrentLabel);
+                    Destroy(CurrentLink);
+                    previousLabel.transform.SetParent(null);
+                    previousLink.transform.SetParent(null);
+                    Destroy(previousLabel);
+                    Destroy(previousLink);
+
+                    break;
+                }
+                yield return new WaitForSeconds(1.5f);
+            }
+
+
+            CurrentLabel.transform.SetParent(null);
+            CurrentLink.transform.SetParent(null);
+            Destroy(CurrentLabel);
+            Destroy(CurrentLink);
+            previousLabel.transform.SetParent(null);
+            previousLink.transform.SetParent(null);
+            Destroy(previousLabel);
+            Destroy(previousLink);
+            
+
+            length++;
         }
     }
 
